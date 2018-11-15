a118e7d3fc695b543b37a05faedc9f4b2891bddb
@serghku <sergei.kustsenko@gmail.com>
2018-11-15 15:41:04 +0200

    login to properties

--- src/main/java/com/tivi/homework/email/EmailService.java
+++ src/main/java/com/tivi/homework/email/EmailService.java
@@ -5,6 +5,10 @@ import com.tivi.homework.model.GameSession;
 import javax.mail.*;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Date;
 import java.util.Properties;
 
@@ -19,20 +23,33 @@ public class EmailService {
         props.put("mail.smtp.host", "smtp.gmail.com");
         props.put("mail.smtp.port", "587");
 
-        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
-            @Override
-            protected PasswordAuthentication getPasswordAuthentication() {
-                return new PasswordAuthentication("sergei.student1@gmail.com", "serghteststudent");
-            }
-        });
-        Message msg = new MimeMessage(session);
+        try {
+            FileReader reader = new FileReader(System.getProperty("user.dir") +
+                    File.separator + "src" +
+                    File.separator + "main" +
+                    File.separator + "resources" +
+                    File.separator + "application.properties");
+            Properties properties = new Properties();
+            properties.load(reader);
+            String gmail = properties.getProperty("gmailUser");
+            String password = properties.getProperty("gmailPassword");
 
-        msg.setFrom(new InternetAddress("sergei.student1@gmail.com", false));
+            Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+                @Override
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(gmail, password);
+                }
+            });
+            Message msg = new MimeMessage(session);
+            msg.setFrom(new InternetAddress("sergei.student1@gmail.com", false));
 
-        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
-        msg.setSubject("KTTT game session : "+gameSession.getId());
-        msg.setContent(content, "text/html");
-        msg.setSentDate(new Date());
-        Transport.send(msg);
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email));
+            msg.setSubject("KTTT game session : " + gameSession.getId());
+            msg.setContent(content, "text/html");
+            msg.setSentDate(new Date());
+            Transport.send(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
